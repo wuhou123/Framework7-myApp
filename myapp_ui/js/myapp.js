3063333761485c59aa43da1e503e0e531dcf0213
YANG FEI <975694783@qq.com>
2017-03-17 22:02:28 +0800

    微信应用

---
+++ myapp_ui/js/myapp.js
@@ -0,0 +1,17 @@
+function type_change(a){
+ if(a == 29){
+ 	return "搞笑段子";
+ }
+ if(a == 41){
+ 	return "搞笑视频";
+ }
+ if(a == 10){
+ 	return "搞笑图片";
+ }
+ if(a ==31){
+ 	return "搞笑声音";
+ }
+ else{
+ 	return a;
+}
+}
